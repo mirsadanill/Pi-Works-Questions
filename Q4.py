4702f6dff9b890f2867a97782ec4551a2c84b17f
Mirsad <93525665+mirsadanill@users.noreply.github.com>
2022-04-16 21:20:15 +0300

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -1,19 +1,21 @@
 import pandas as pd
 import numpy as np
 df = pd.read_csv("./country_vaccination_stats.csv")
-df['date'] = pd.to_datetime(df['date'])
 
 
 min=df.groupby('country').min().reset_index().set_index('country')['daily_vaccinations']
+print(min)
 
 NaN=np.where(df.daily_vaccinations.isnull()) 
-
+print(NaN)
 
 for i in NaN:
-    df.daily_vaccinations[i]=min[df.country[i]] 
-
+    df["daily_vaccinations"][i]=min[df['country'][i]] 
 
 NaNForNoValues=np.where(df.daily_vaccinations.isnull())
+
 if(len(NaNForNoValues)):
-    for y in NaNForNoValues:
+    for i in NaNForNoValues:
         df.daily_vaccinations[i]=0
+
+print(df)
