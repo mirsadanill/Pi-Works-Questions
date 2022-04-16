015f97b6f004269c057c7f5024d7ed6889b956ae
Mirsad <93525665+mirsadanill@users.noreply.github.com>
2022-04-16 21:11:16 +0300

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -1,6 +1,19 @@
 import pandas as pd
+import numpy as np
+df = pd.read_csv("./country_vaccination_stats.csv")
+df['date'] = pd.to_datetime(df['date'])
 
-excel_file= "./country_vaccination_stats.csv"
-df=pd.read_csv(excel_file)
-df.fillna(value=0,inplace=True)
-print(df.info())
+
+min=df.groupby('country').min().reset_index().set_index('country')['daily_vaccinations']
+
+NaN=np.where(df.daily_vaccinations.isnull()) 
+
+
+for i in NaN:
+    df.daily_vaccinations[i]=min[df.country[i]] 
+
+
+NaNForNoValues=np.where(df.daily_vaccinations.isnull())
+if(len(NaNForNoValues)):
+    for y in NaNForNoValues:
+        df.daily_vaccinations[y]=0
