bac52efbaa42f997e9cd12531219e1fc7767c32b
Mirsad <93525665+mirsadanill@users.noreply.github.com>
2022-04-16 21:39:14 +0300

    Update Q5.py

--- Q5.py
+++ Q5.py
@@ -5,16 +5,16 @@ df = pd.read_csv("./country_vaccination_stats.csv")
 
 min=df.groupby('country')['daily_vaccinations'].min()
 
-NaN=np.where(df.daily_vaccinations.isnull()) 
+NaN=np.where(df['daily_vaccinations'].isnull()) 
 
 for i in NaN:
     df["daily_vaccinations"][i]=min[df['country'][i]] 
 
-NaNForNoValues=np.where(df.daily_vaccinations.isnull())
+NaNForNoValues=np.where(df['daily_vaccinations'].isnull())
 
 if(len(NaNForNoValues)):
     for i in NaNForNoValues:
-        df.daily_vaccinations[i]=0
+        df['daily_vaccinations'][i]=0
 
 country_median = df.groupby('country').median()
 top_3_country = country_median.sort_values('daily_vaccinations', ascending=False).iloc[:3]
