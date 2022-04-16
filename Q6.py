67908831ad4d0ef281c4f7a30299e87c6e25b755
Mirsad <93525665+mirsadanill@users.noreply.github.com>
2022-04-16 21:23:40 +0300

    Update Q6.py

--- Q6.py
+++ Q6.py
@@ -1,7 +1,21 @@
 import pandas as pd
+import numpy as np
+df = pd.read_csv("./country_vaccination_stats.csv")
 
-excel_file= "./country_vaccination_stats.csv"
-df=pd.read_csv(excel_file)
-df.fillna(value=0,inplace=True)
+
+min=df.groupby('country').min().reset_index().set_index('country')['daily_vaccinations']
+#print(min)
+
+NaN=np.where(df.daily_vaccinations.isnull()) 
+#print(NaN)
+
+for i in NaN:
+    df["daily_vaccinations"][i]=min[df['country'][i]] 
+
+NaNForNoValues=np.where(df.daily_vaccinations.isnull())
+
+if(len(NaNForNoValues)):
+    for i in NaNForNoValues:
+        df.daily_vaccinations[i]=0
 
 print(df[df['date']=='1/6/2021']['daily_vaccinations'].sum())
